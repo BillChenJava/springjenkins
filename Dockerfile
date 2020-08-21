a04ccafd52c97f977bf8569a0f55f4ad9d2aef9a
BillChenJava <58630964+BillChenJava@users.noreply.github.com>
2020-08-21 10:33:44 -0400

    test

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,5 @@ RUN apt-get update
 RUN apt-get install vim -y
 RUN mkdir /springjenkins
 ADD ${JAR_FILE} /springjenkins
-EXPOSE 9090
+EXPOSE 9081
 ENTRYPOINT ["java","-jar","/springjenkins/springjenkins-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/mcd/springjenkins/controller/TestController.java
+++ src/main/java/com/mcd/springjenkins/controller/TestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("")
     public ResponseEntity<?> getTest(){
-        String result = "springjenkins";
+        String result = "springjenkinstest";
         return ResponseEntity.ok(result);
     }
 }
