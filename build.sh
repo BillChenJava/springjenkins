0ec06b9a3baf424f10a00a49203bfd790216d514
BillChenJava <58630964+BillChenJava@users.noreply.github.com>
2020-08-23 10:21:37 -0400

    get all userstest

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-mvn clean
-mvn package -DskipTests
-docker rmi -f springjenkins:20190628-1.0
-mvn Dockerfile:build
-docker images
+sudo mvn clean
+sudo mvn package -DskipTests
+sudo docker rmi -f springjenkins:20190628-1.0
+sudo mvn Dockerfile:build
+sudo docker images
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-docker ps -a
-docker stop ruleparser
-docker rm -f ruleparser
-docker run -d --name ruleparser --network jids --network-alias jids -p 9081:9081 -v /var/jenkins_home/workspace/java_tale/data:/data -v /etc/localtime:/etc/localtime -v /etc/timezone:/etc/timezone ruleparser:20190628-1.0
+sudo docker ps -a
+sudo docker stop ruleparser
+sudo docker rm -f ruleparser
+sudo docker run -d --name ruleparser --network jids --network-alias jids -p 9081:9081 -v /var/jenkins_home/workspace/java_tale/data:/data -v /etc/localtime:/etc/localtime -v /etc/timezone:/etc/timezone ruleparser:20190628-1.0
